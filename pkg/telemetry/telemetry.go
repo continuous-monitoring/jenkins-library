1121b0f836227b6a2bc92837e7ee80b343e79344
aditya161989 <aditya.bikram.pattnaik@sap.com>
2021-01-18 21:24:51 +0530

    Update telemetry.go

--- pkg/telemetry/telemetry.go
+++ pkg/telemetry/telemetry.go
@@ -63,7 +63,7 @@ func Initialize(telemetryDisabled bool, stepName string) {
 	}
 	//ToDo: register Logrus Hook
 
-	httpHook := &log.HTTPHook{correlationID: GeneralConfig.CorrelationID, pipelineURLHash: getPipelineURLHash(), buildURLHash: getBuildURLHash()}
+	httpHook := &log.HTTPHook{CorrelationID: GeneralConfig.CorrelationID, PipelineURLHash: getPipelineURLHash(), BuildURLHash: getBuildURLHash()}
 	log.RegisterHook(httpHook)
 	log.SetVerbose(true)
 }
