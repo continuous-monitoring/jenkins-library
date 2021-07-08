c9b8328724552c17bd73b108f3c755967556af27
Felix Feldmann <F.Feldmann@sap.com>
2021-07-08 10:41:26 +0200

    Re-use dummy function

--- vars/influxWriteData.groovy
+++ vars/influxWriteData.groovy
@@ -89,7 +89,7 @@ void call(Map parameters = [:]) {
             .mixinStageConfig(script.commonPipelineEnvironment, stageName, STEP_CONFIG_KEYS)
             .mixin([
                 artifactVersion: script.commonPipelineEnvironment.getArtifactVersion(),
-                commitHash: script.commonPipelineEnvironment.getCommitHash(),
+                commitHash: script.commonPipelineEnvironment.getArtifactVersion(),
                 influxPrefix: script.commonPipelineEnvironment.getGithubOrg() && script.commonPipelineEnvironment.getGithubRepo()
                     ? "${script.commonPipelineEnvironment.getGithubOrg()}_${script.commonPipelineEnvironment.getGithubRepo()}"
                     : null
